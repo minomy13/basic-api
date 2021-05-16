65b2a907d54c6bde23272363ccf635bb548c83ca
minomy13 <mail@minomy13.de>
2021-05-16 17:47:08 +0200

    imported and middlewared auth route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+// Routes Import
+const authRoute = require('./routes/auth');
+
+// Routes Middleware
+app.use('/api/user', authRoute);
+
 app.listen(3000, console.log('Running.'));
