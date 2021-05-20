4c72f6019bb2c8c990efd80daf4b17138e8a31e1
minomy13 <mail@minomy13.de>
2021-05-20 20:40:45 +0200

    mongoose connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Config Import
+const tokens = require('./config/tokens.json');
+
+// DB Connection
+mongoose.connect(tokens.mongodb);
 
 // Routes Import
 const authRoute = require('./routes/auth');
