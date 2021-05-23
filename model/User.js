cd4a50bae0ddb91809a93f745544e321d7bb6e3b
minomy13 <mail@minomy13.de>
2021-05-23 14:46:32 +0200

    created user schema

---
+++ model/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    mobile: {
+      type: String,
+      required: true,
+    },
+    private: {
+      type: String,
+    },
+    bussiness: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
