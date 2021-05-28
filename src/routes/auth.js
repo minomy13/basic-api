aef37b1c4aa163a87e684e7b2dc3db560cd471ad
minomy13 <mail@minomy13.de>
2021-05-28 15:12:20 +0200

    data from post into model

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
 router.post('/register', (req, res) => {
-  res.send('Register');
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    phone: {
+      mobile: req.body.phone.mobile,
+    },
+    birthday: new Date(req.body.birthday),
+  });
 });
 
 module.exports = router;
