6c34f221e9bc2a6ef59b41bcf834a293d7ed67df
minomy13 <mail@minomy13.de>
2021-05-30 01:31:36 +0200

    token verification

---
+++ src/verifyToken.js
@@ -0,0 +1,18 @@
+// Module Imports
+const jwt = require('jsonwebtoken');
+
+// Config Import
+const tokens = require('../config/tokens.json');
+
+function auth(req, res, next) {
+  // Check For Token
+  const token = req.header.authentication;
+  if (!token) return res.status(401).send('Access denied.');
+
+  // Verify
+  try {
+    const verified = jwt.verify(token, tokens.TOKEN_SECRET);
+  } catch (err) {
+    res.status(400).send('Invalid token.');
+  }
+}
