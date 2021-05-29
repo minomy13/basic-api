e9fde50a1747659e8215cafad39306814a87738c
minomy13 <mail@minomy13.de>
2021-05-29 16:56:35 +0200

    added min and max

--- src/model/User.js
+++ src/model/User.js
@@ -4,25 +4,37 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    min: 6,
+    max: 255,
   },
   email: {
     type: String,
     required: true,
+    min: 6,
+    max: 255,
   },
   password: {
     type: String,
     required: true,
+    min: 6,
+    max: 1024,
   },
   phone: {
     mobile: {
       type: String,
       required: true,
+      min: 6,
+      max: 255,
     },
     private: {
       type: String,
+      min: 6,
+      max: 255,
     },
     bussiness: {
       type: String,
+      min: 6,
+      max: 255,
     },
   },
   birthday: {
