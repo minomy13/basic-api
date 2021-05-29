3975f1e6fa82c618bf698ddb9c3c98d7370f53ef
minomy13 <mail@minomy13.de>
2021-05-29 18:02:13 +0200

    login validation

--- src/validation.js
+++ src/validation.js
@@ -16,5 +16,16 @@ const registerValidation = (data) => {
   return Joi.validate(data, schema);
 };
 
+// Login
+const loginValidation = (data) => {
+  const schema = {
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  };
+
+  return Joi.validate(data, schema);
+};
+
 // Exports
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
